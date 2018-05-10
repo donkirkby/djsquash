00744cdc43ede9ecef4bbe429c6fa24c79a45a51
don <donkirkby@gmail.com>
2018-05-10 14:14:35 -0700

    Create a new set of squashed migrations for Django 1.11.

--- fruit/migrations/0003_apple_size.py
+++ fruit/migrations/0003_apple_size.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('fruit', '0002_cranberry_bacon'),
+        ('fruit', '0001_initial'),
     ]
 
     operations = [
---
+++ fruit/migrations/0100_prepare_squash.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fruit', '0003_apple_size'),
+        ('fruit', '0002_cranberry_bacon'),
+    ]
+
+    operations = [
+    ]
